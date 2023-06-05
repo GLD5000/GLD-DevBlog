37a05baab602b31ac2d002c977ba5ca9339eb9a5
GLD5000 <garethlouisdevlin@gmail.com>
2023-06-05 22:28:43 +0100

    Added Next Auth route

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,10 +4,13 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma =
+const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ["query"],
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+
+export default prisma;
