c9798193a864b37f99929681dea42d6a9d04717f
GLD5000 <garethlouisdevlin@gmail.com>
2023-06-06 16:15:06 +0100

    Wrapped session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,29 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "GLD DevBlog",
-  description:
-    "Catch up on the lates web developer trends and hot takes.",
+  description: "Catch up on the lates web developer trends and hot takes.",
   keywords:
     "Blog, React, web, coding, developer, HTML, CSS, JavaScript, TypeScript",
   authors: [{ name: "GLD", url: "https://github.com/GLD5000" }],
   manifest: "/manifest.json",
   icons: { icon: "/gldcondensed.ico" },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SessionProvider>{children}</SessionProvider>
+      </body>
     </html>
-  )
+  );
 }
