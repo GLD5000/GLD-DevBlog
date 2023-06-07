1fb327a17b06c87b964388b4395524d4885689bd
GLD5000 <garethlouisdevlin@gmail.com>
2023-06-07 16:38:25 +0100

    Updated delete route

--- src/app/api/delete/[id]/route.ts
+++ src/app/api/delete/[id]/route.ts
@@ -2,13 +2,14 @@ import prisma from "@/lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 
 // DELETE /api/post/:id
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const postId = req.query.id as string;
+async function handler(req: Request, res: Response) {
+  const post = await req.json();
+  const postId = post.id;
   if (req.method === "DELETE") {
     const post = await prisma.post.delete({
       where: { id: postId },
     });
-    res.json(post);
+    return new Response(JSON.stringify(post));
   } else {
     throw new Error(
       `The HTTP ${req.method} method is not supported at this route.`
