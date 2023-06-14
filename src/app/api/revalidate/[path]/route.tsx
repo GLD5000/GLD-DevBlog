917734b621bd8c41c60399632ca9ffeacaf352b8
GLD5000 <garethlouisdevlin@gmail.com>
2023-06-14 11:51:50 +0100

    Removed comments

--- src/app/api/revalidate/[path]/route.tsx
+++ src/app/api/revalidate/[path]/route.tsx
@@ -1,12 +1,12 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
-export async function GET(req: Request,context: {[key:string]: {[key:string]: string}}) {
+export async function GET(
+  req: Request,
+  context: { [key: string]: { [key: string]: string } }
+) {
   const pathIn = context.params.path;
-  const path  = pathIn? `/${pathIn}/`: '/';
-  // console.log('path:', path);
-  //   const path = request.nextUrl.searchParams.get('path') || '/'
-  //   revalidatePath(path);
+  const path = pathIn ? `/${pathIn}/` : "/";
   revalidatePath(path);
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
