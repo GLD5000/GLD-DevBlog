f068393a1996dc52792e4dafb32f1a429bed3677
GLD5000 <garethlouisdevlin@gmail.com>
2023-06-24 13:58:10 +0100

    Added read time util

---
+++ src/utilities/readTime.ts
@@ -0,0 +1,6 @@
+export default function getReadTime(markdown: string){
+    const WORDS_PER_MIN = 180;
+    const words = markdown.replaceAll(/[^\w\s]/g,'').trim().split(/\s+/).length;
+    const time = Math.ceil(words /WORDS_PER_MIN);
+    return time;
+}
