4a274ad3cfbaccc600fe7746821187b7b2d988ff
GLD5000 <garethlouisdevlin@gmail.com>
2023-06-26 17:19:21 +0100

    Added secret to revalidate

--- src/app/api/revalidate/[path]/route.tsx
+++ src/app/api/revalidate/[path]/route.tsx
@@ -1,12 +1,22 @@
-import { NextResponse } from "next/server";
-import { revalidatePath } from "next/cache";
+import { NextRequest, NextResponse } from 'next/server'
+import { revalidatePath } from 'next/cache'
 
-export async function GET(
-  req: Request,
-  context: { [key: string]: { [key: string]: string } }
-) {
-  const pathIn = context.params.path;
-  const path = pathIn ? `/${pathIn}/` : "/";
-  revalidatePath(path);
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+export async function GET(request: NextRequest) {
+    const secret = request.nextUrl.searchParams.get('secret')
+
+    if (secret !== process.env.MY_SECRET_TOKEN) {
+        return new NextResponse(JSON.stringify({ message: 'Invalid Token' }), {
+            status: 401,
+            statusText: 'Unauthorized',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+
+    const path = request.nextUrl.searchParams.get('path') || '/'
+
+    revalidatePath(path)
+
+    return NextResponse.json({ revalidated: true })
 }
