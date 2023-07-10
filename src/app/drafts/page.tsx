f1d4dc813be09977bca85feca541efb649bb2ecf
GLD5000 <garethlouisdevlin@gmail.com>
2023-07-10 16:30:27 +0100

    Reinstate SSR

--- src/app/drafts/page.tsx
+++ src/app/drafts/page.tsx
@@ -1,49 +1,79 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
-import { PostEmailProps } from "@/components/BlogPost";
-import { useSession } from "next-auth/react";
+import React from "react";
+import BlogPost, { PostProps } from "@/components/BlogPost";
+import { Session, getServerSession } from "next-auth";
+import prisma from "@/lib/prisma";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import BlogPostList from "@/components/BlogPostList";
 
-export default function Drafts() {
-  const { data: session } = useSession();
-  const [data, setData] = useState<{
-    drafts: PostEmailProps[];
-    published: PostEmailProps[];
-  }>();
+const getData = async (
+  sessionData: Session
+): Promise<{
+  published: PostProps[];
+  drafts: PostProps[];
+}> => {
+  const drafts: PostProps[] = await prisma.post.findMany({
+    where: {
+      author: { email: sessionData?.user?.email },
+      published: false,
+    },
+    orderBy: { createdAt: "desc" },
+    include: {
+      tags: {
+        orderBy: { tag: { name: "asc" } },
+        select: { tag: true },
+      },
+      author: {
+        select: { name: true, email: true },
+      },
+    },
+  });
+  const published: PostProps[] = await prisma.post.findMany({
+    where: {
+      author: { email: sessionData?.user?.email },
+      published: true,
+    },
+    orderBy: { createdAt: "desc" },
+    include: {
+      tags: {
+        orderBy: { tag: { name: "asc" } },
+        select: { tag: true },
+      },
+      author: {
+        select: { name: true, email: true },
+      },
+    },
+  });
 
-  useEffect(() => {
-    const getPosts = async () => {
-      try {
-        const response = await fetch(
-          `/api/get?authorEmail=${session?.user?.email}`
-        );
-        const responseData = await response.json();
-        setData(responseData);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  return {
+    published,
+    drafts,
+  };
+};
 
-    if (session) {
-      getPosts();
-    }
-  }, [session]);
-  if (session === null)
+const Drafts = async () => {
+  const session = await getServerSession(authOptions);
+  if (!session) {
     return (
-      <h1 className="mx-auto w-fit text-txt-main dark:text-txt-main-dk">
-        You need to be authenticated to view this page.
-      </h1>
+      <div className="prose mx-auto grid gap-8 py-8 dark:prose-invert">
+        <h1 className="mx-auto text-txt-main dark:text-txt-main-dk">
+          My Blogs
+        </h1>
+        <div className="mx-auto text-txt-main dark:text-txt-main-dk">
+          You need to be authenticated to view this page.
+        </div>
+      </div>
     );
+  }
+  const { drafts, published } = await getData(session);
 
   return (
     <div className="prose mx-auto grid gap-8 py-8 dark:prose-invert">
-      {data?.published && data?.published.length > 0 ? (
+      {published.length ? (
         <>
           <h1 className="mx-auto w-fit text-txt-main dark:text-txt-main-dk">
-            My Published Blogs
+            Published
           </h1>
-          <BlogPostList arrayIn={data?.published} />
+          <BlogPostList arrayIn={...published} />
         </>
       ) : (
         <h2 className="mx-auto w-fit text-txt-main dark:text-txt-main-dk">
@@ -51,12 +81,12 @@ export default function Drafts() {
         </h2>
       )}
 
-      {data?.drafts && data?.drafts.length > 0 ? (
+      {drafts.length ? (
         <>
           <h1 className="mx-auto w-fit text-txt-main dark:text-txt-main-dk">
-            My Drafts
+            Drafts
           </h1>
-          <BlogPostList arrayIn={data?.drafts} />
+          <BlogPostList arrayIn={...drafts} />
         </>
       ) : (
         <h2 className="mx-auto w-fit text-txt-main dark:text-txt-main-dk">
@@ -65,4 +95,6 @@ export default function Drafts() {
       )}
     </div>
   );
-}
+};
+
+export default Drafts;
