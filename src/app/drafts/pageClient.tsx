e1f69d035ddf6ebbb9b890c45f6d74dcc3b4a7eb
GLD5000 <garethlouisdevlin@gmail.com>
2023-07-10 16:24:44 +0100

    backup draft page

---
+++ src/app/drafts/pageClient.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { PostEmailProps } from "@/components/BlogPost";
+import { useSession } from "next-auth/react";
+import BlogPostList from "@/components/BlogPostList";
+
+export default function Drafts() {
+  const { data: session } = useSession();
+  const [data, setData] = useState<{
+    drafts: PostEmailProps[];
+    published: PostEmailProps[];
+  }>();
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const response = await fetch(
+          `/api/get?authorEmail=${session?.user?.email}`
+        );
+        const responseData = await response.json();
+        setData(responseData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (session) {
+      getPosts();
+    }
+  }, [session]);
+  if (session === null)
+    return (
+      <h1 className="mx-auto w-fit text-txt-main dark:text-txt-main-dk">
+        You need to be authenticated to view this page.
+      </h1>
+    );
+
+  return (
+    <div className="prose mx-auto grid gap-8 py-8 dark:prose-invert">
+      {data?.published && data?.published.length > 0 ? (
+        <>
+          <h1 className="mx-auto w-fit text-txt-main dark:text-txt-main-dk">
+            My Published Blogs
+          </h1>
+          <BlogPostList arrayIn={data?.published} />
+        </>
+      ) : (
+        <h2 className="mx-auto w-fit text-txt-main dark:text-txt-main-dk">
+          No Published Blogs Yet
+        </h2>
+      )}
+
+      {data?.drafts && data?.drafts.length > 0 ? (
+        <>
+          <h1 className="mx-auto w-fit text-txt-main dark:text-txt-main-dk">
+            My Drafts
+          </h1>
+          <BlogPostList arrayIn={data?.drafts} />
+        </>
+      ) : (
+        <h2 className="mx-auto w-fit text-txt-main dark:text-txt-main-dk">
+          No Drafts Yet
+        </h2>
+      )}
+    </div>
+  );
+}
