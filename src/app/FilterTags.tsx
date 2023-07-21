67c7e3b76c30f2717c2acd3c1f7217505f608f9c
GLD5000 <garethlouisdevlin@gmail.com>
2023-07-21 14:57:57 +0100

    Add tags to filter tags search bar #42

--- src/app/FilterTags.tsx
+++ src/app/FilterTags.tsx
@@ -1,16 +1,27 @@
 "use client";
 
 import { useStore } from "@/store/zustand";
+import { Tag } from "@prisma/client";
+
+function getTagOptionList(tagList: Tag[]) {
+  return tagList.map((tag) => <option key={tag.id}>{tag.name}</option>);
+}
 
 export default function FilterTags() {
-  const { searchTags } = useStore();
+  const { searchTags, allTags } = useStore();
+  console.log("allTags:", allTags);
+  const listOptions = getTagOptionList(allTags);
   return (
-    <input
-      onChange={(e) => useStore.setState({ searchTags: e.target.value })}
-      placeholder="Filter Tags e.g.: Typescript React"
-      type="text"
-      value={searchTags}
-      className="w-full shrink grow break-words rounded bg-bg-var p-2 text-txt-main dark:bg-bg-var-dk dark:text-txt-main-dk"
-    />
+    <>
+      <input
+        onChange={(e) => useStore.setState({ searchTags: e.target.value })}
+        placeholder="Filter Tags e.g.: Typescript React"
+        type="search"
+        value={searchTags}
+        className="w-full shrink grow break-words rounded bg-bg-var p-2 text-txt-main dark:bg-bg-var-dk dark:text-txt-main-dk"
+        list="filter-tags-list"
+      />
+      <datalist id="filter-tags-list">{listOptions}</datalist>
+    </>
   );
 }
