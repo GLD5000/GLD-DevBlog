540ee5b58e39e8075799022fb5be10c65f3bec7c
GLD5000 <garethlouisdevlin@gmail.com>
2023-07-29 17:38:46 +0100

    Add tag reducer logic #47

--- src/lib/redux/slices/formSlice/formSlice.ts
+++ src/lib/redux/slices/formSlice/formSlice.ts
@@ -1,5 +1,6 @@
 /* Core */
 import { createSlice } from "@reduxjs/toolkit";
+import getRandomColour from "@/utilities/colour/randomColour";
 import { FormSliceState } from "./types";
 import { saveField, saveTags } from "./localStorage";
 
@@ -28,9 +29,12 @@ const formSlice = createSlice({
       const currentTags = state.tags;
       const shouldAddTag = testToAddTag(currentString, currentTags);
       if (shouldAddTag) {
-        // push to tags
+        const { tags, tagString } = addNewTag(currentString, currentTags);
+        saveField("tagString", { tagString });
+        saveTags(tags);
+        return { tags, tagString };
       }
-      saveTags(action.payload);
+      saveField("tagString", action.payload);
       return { ...state, ...action.payload };
     },
   },
@@ -54,4 +58,10 @@ function testToAddTag(
     /[ ,.]/.test(`${currentString.at(-1)}`) && currentString.length > 1;
   const tagsHaveSpace = currentTags === undefined || currentTags.size < 5;
   return stringComplete && tagsHaveSpace;
+}
+
+function addNewTag(stringIn: string, tagsFromState: FormSliceState["tags"]) {
+  const newTags = tagsFromState ? new Map(tagsFromState) : new Map();
+  newTags.set(stringIn.trim(), getRandomColour("mid"));
+  return { tags: newTags, tagString: "" };
 }
