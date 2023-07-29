4352ce2ab8627d3222b166007ff17db9d42853cd
GLD5000 <garethlouisdevlin@gmail.com>
2023-07-29 15:17:29 +0100

    Add local storage module for Form State

---
+++ src/utilities/state/localStorage.ts
@@ -0,0 +1,45 @@
+import { FormSliceState } from "@/lib/redux/slices/inputFormSlice";
+
+function stringifyForm(formData: FormSliceState) {
+  return JSON.stringify({
+    ...formData,
+    tags: formData.tags ? Array.from(formData.tags) : undefined,
+  });
+}
+
+function parseForm(formData: string) {
+  const returnedObj = JSON.parse(formData);
+  const tagsArray = returnedObj.tags ? Array.from(returnedObj.tags) : undefined;
+  const tagsMap = tagsArray
+    ? new Map(tagsArray as [string, string][])
+    : undefined;
+  const initialObject = {
+    title: returnedObj.title || "",
+    content: returnedObj.content || "",
+    publish: returnedObj.publish || false,
+    tags: tagsMap,
+    tagString: returnedObj.tagString || "",
+  };
+  return initialObject;
+}
+
+export function saveForm(state: FormSliceState) {
+  try {
+    const serializedState = stringifyForm(state);
+    localStorage.setItem("inputForm", serializedState);
+  } catch {
+    // ignore write errors
+  }
+}
+
+export function loadForm() {
+  try {
+    const serializedState = localStorage.getItem("inputForm");
+    if (serializedState === null) {
+      return undefined;
+    }
+    return parseForm(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+}
