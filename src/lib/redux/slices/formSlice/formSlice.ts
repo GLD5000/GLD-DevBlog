a22f673d4331b3b36a6711aa19604306f55a05bf
GLD5000 <garethlouisdevlin@gmail.com>
2023-07-29 22:29:51 +0100

    Reordered functions

--- src/lib/redux/slices/formSlice/formSlice.ts
+++ src/lib/redux/slices/formSlice/formSlice.ts
@@ -12,6 +12,24 @@ const initialState: FormSliceState = {
   tags: undefined,
   tagString: "",
 };
+function testToAddTag(
+  currentString: any,
+  currentTags: Map<string, string> | undefined
+) {
+  const stringComplete =
+    /[ ,.]/.test(`${currentString.at(-1)}`) && currentString.length > 1;
+  const tagsHaveSpace = currentTags === undefined || currentTags.size < 5;
+  return stringComplete && tagsHaveSpace;
+}
+
+function getUpdatedTags(
+  stringIn: string,
+  tagsFromState: FormSliceState["tags"]
+) {
+  const newTags = tagsFromState ? new Map(tagsFromState) : new Map();
+  newTags.set(stringIn.trim(), getRandomColour("mid"));
+  return { tags: newTags, tagString: "" };
+}
 
 const formSlice = createSlice({
   name: "counter",
@@ -26,7 +44,7 @@ const formSlice = createSlice({
       const currentTags = state.tags;
       const shouldAddTag = testToAddTag(currentString, currentTags);
       if (shouldAddTag) {
-        const { tags, tagString } = addNewTag(currentString, currentTags);
+        const { tags, tagString } = getUpdatedTags(currentString, currentTags);
         saveField("tagString", { tagString });
         saveTags(tags);
         return { tags, tagString };
@@ -40,19 +58,3 @@ const formSlice = createSlice({
 const { actions, reducer } = formSlice;
 export const { updateField } = actions;
 export { reducer as FormReducer };
-
-function testToAddTag(
-  currentString: any,
-  currentTags: Map<string, string> | undefined
-) {
-  const stringComplete =
-    /[ ,.]/.test(`${currentString.at(-1)}`) && currentString.length > 1;
-  const tagsHaveSpace = currentTags === undefined || currentTags.size < 5;
-  return stringComplete && tagsHaveSpace;
-}
-
-function addNewTag(stringIn: string, tagsFromState: FormSliceState["tags"]) {
-  const newTags = tagsFromState ? new Map(tagsFromState) : new Map();
-  newTags.set(stringIn.trim(), getRandomColour("mid"));
-  return { tags: newTags, tagString: "" };
-}
