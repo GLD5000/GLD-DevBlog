f4a7e6b7fa2dbd8e6f6f9f6ca919925c80754da4
GLD5000 <garethlouisdevlin@gmail.com>
2023-08-01 23:58:26 +0100

    Clean-up comments

--- src/utilities/colour/newTagMaker.ts
+++ src/utilities/colour/newTagMaker.ts
@@ -1,11 +1,8 @@
 export default function makeNewTag(tagArray: string[]) {
-  // mid colour lum 17.6 - 18.1%
   const backgroundColour = tagArray[1];
-  // console.log('backgroundColour:', backgroundColour);
   const newTag = {
     name: tagArray[0].trim(),
     backgroundColour,
   };
-
   return newTag;
 }
